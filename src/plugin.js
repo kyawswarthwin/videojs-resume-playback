61e94d2a0edaee28a84da7031c7d0938901e8af1
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2023-08-17 18:15:47 +0630

    features added

--- src/plugin.js
+++ src/plugin.js
@@ -1,50 +1,64 @@
 import videojs from 'video.js';
-import {version as VERSION} from '../package.json';
+import { version as VERSION } from '../package.json';
 
 const Plugin = videojs.getPlugin('plugin');
+const Button = videojs.getComponent('Button');
 
-// Default options for the plugin.
-const defaults = {};
+const defaults = {
+  resumeable: true,
+};
 
-/**
- * An advanced Video.js plugin. For more information on the API
- *
- * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
- */
-class ResumePlayback extends Plugin {
-
-  /**
-   * Create a ResumePlayback plugin instance.
-   *
-   * @param  {Player} player
-   *         A Video.js Player instance.
-   *
-   * @param  {Object} [options]
-   *         An optional options object.
-   *
-   *         While not a core part of the Video.js plugin architecture, a
-   *         second argument of options is a convenient way to accept inputs
-   *         from your plugin's caller.
-   */
+class ReplayButton extends Button {
   constructor(player, options) {
-    // the parent class will add player under this.player
+    super(player, options);
+    this.controlText('Replay');
+  }
+
+  buildCSSClass() {
+    return `vjs-replay-control ${super.buildCSSClass()}`;
+  }
+
+  handleClick(event) {
+    this.player_.currentTime(0);
+    this.player_.play();
+  }
+}
+videojs.registerComponent('ReplayButton', ReplayButton);
+
+class ResumePlayback extends Plugin {
+  constructor(player, options) {
     super(player);
 
     this.options = videojs.mergeOptions(defaults, options);
 
+    const { resumeable, videoId } = this.options;
+
+    this.player.controlBar.addChild('ReplayButton', {}, 0);
+
     this.player.ready(() => {
-      this.player.addClass('vjs-resume-playback');
+      const key = `videojs-resume-playback:${videoId}`;
+
+      this.player.on('timeupdate', () => {
+        localStorage.setItem(key, this.player.currentTime());
+      });
+
+      this.player.on('ended', () => {
+        localStorage.removeItem(key);
+      });
+
+      const lastTime = parseFloat(localStorage.getItem(key));
+      if (resumeable && lastTime) {
+        this.player.currentTime(lastTime);
+        this.player.play();
+      }
     });
   }
 }
 
-// Define default values for the plugin's `state` object here.
 ResumePlayback.defaultState = {};
 
-// Include the version number.
 ResumePlayback.VERSION = VERSION;
 
-// Register the plugin with video.js.
 videojs.registerPlugin('resumePlayback', ResumePlayback);
 
 export default ResumePlayback;
